16eb289e3c9247fb690eaa548c435d2bcdc34a64
wisnuciwun <adiwardhanawisnu@gmail.com>
2022-05-10 06:57:48 +0700

    learn part 1 - initial and folder structure (styles & components)

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Welcome from './src/components/Welcome';
+import { Typography } from './src/styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Typography.style.container}>
+      <Welcome/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function Welcome() {
+     return (
+          <>
+               <Text>Hi all ! This is my first react native app with expo</Text>
+               <StatusBar style="auto" />
+          </>
+     );
+}
